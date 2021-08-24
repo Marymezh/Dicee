98bb3e1a229fbe613ea1c4c14948ac5f9ce9d5e3
Marymezh <maria.mezhova@yahoo.com>
2021-08-24 15:30:05 +0700

    created Dicee app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
+        
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+        
+        // another way to randomize a particular array
+//        diceImageViewOne.image = diceArray.randomElement()
+//        diceImageViewTwo.image = diceArray.randomElement()
+       
+        
     }
-
-
+    
+    
 }
 
